4f5199b579acf90d588647265b8df001daace310
allison0110 <ayss3310@gmail.com>
2022-05-11 16:25:56 +0900

    필요없는 폴더 삭제 (main병합)

--- SemiProject/WebContent/sql/apc_category.sql
+++
@@ -1,8 +0,0 @@
--- apc_category 카테고리 테이블
-
-create table apc_category(
-
-	category_no number(5) primary key, 	-- 카테고리 번호
-	category_code varchar2(8) not null,		-- 카테고리 코드
-	category_name varchar2(100) not null	-- 카테고리 이름
-);
--- SemiProject_Team3/WebContent/sql/apc_category.sql
+++
@@ -1,8 +0,0 @@
--- apc_category 카테고리 테이블
-
-create table apc_category(
-
-	category_no number(5) not null, 	    -- 카테고리 대분류 (1 Women 2 Men 3 Golf)
-	category_code varchar2(8) primary key,  -- 카테고리 코드(중분류)
-	category_name varchar2(100) not null	-- 카테고리 이름 
-);
